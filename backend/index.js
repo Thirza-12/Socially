a8f562220ee419f1b6f92b388fae64b8ba17e77c
Thirza-12 <thirzabehal@gmail.com>
2024-12-18 21:46:06 +0530

    Fixed vite issue

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 
 const corsOptions = {
-    origin: "http://localhost:5173",  // Replace with your frontend URL
+    origin: ["http://localhost:5173", 'https://socially-e6we.onrender.com'],  // Replace with your frontend URL
     credentials: true
 };
 app.use(cors(corsOptions));
