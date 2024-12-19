53c4526bba347ee9e7761b2224b8316035e5c5f6
Thirza-12 <thirzabehal@gmail.com>
2024-12-19 08:36:11 +0530

    Socket io link updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -63,7 +63,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     if (user) {
-      const socketio = io("http://localhost:8000", {
+      const socketio = io("https://socially-yvkc.onrender.com", {
         query: {
           userId: user?._id,
         },
